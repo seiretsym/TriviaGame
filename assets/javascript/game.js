4d3a9b67db1df6531fc6df861ccad02b3b2081d0
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-06 17:54:45 -0700

    fix filename and add music array to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+//// begin trivia code
+
+// variables
+
+// music array
+var songs = [
+    {
+        title: "Main Theme",
+        src: "assets/music/01_main_theme.flac"
+    },
+    {
+        title: "Ophilia, the Cleric",
+        src: "assets/music/02_ophilia_the_cleric.flac"
+    },
+    {
+        title: "Cyrus, the Scholar",
+        src: "assets/music/03_cyrus_the_scholar.flac"
+    },
+    {
+        title: "Tressa, the Merchant",
+        src: "assets/music/04_tressa_the_merchant.flac"
+    },
+    {
+        title: "Olberic, the Warrior",
+        src: "assets/music/05_olberic_the_warrior.flac"
+    },
+    {
+        title: "Primrose, the Dancer",
+        src: "assets/music/06_primrose_the_dancer.flac"
+    },
+    {
+        title: "Alfyn, the Apothecary",
+        src: "assets/music/07_alfyn_the_apothecary.flac"
+    },
+    {
+        title: "Therion, the Thief",
+        src: "assets/music/08_therion_the_thief.flac"
+    },
+    {
+        title: "H'aanit, the Hunter",
+        src: "assets/music/09_haanit_the_hunter.flac"
+    },
+    {
+        title: "The Frostlands",
+        src: "assets/music/10_the_frostlands.flac"
+    },
+    {
+        title: "The Flatlands",
+        src: "assets/music/11_the_flatlands.flac"
+    },
+    {
+        title: "The Coastlands",
+        src: "assets/music/12_the_coastlands.flac"
+    },
+    {
+        title: "The Highlands",
+        src: "assets/music/13_the_highlands.flac"
+    },
+    {
+        title: "The Sunlands",
+        src: "assets/music/14_the_sunlands.flac"
+    },
+    {
+        title: "The Riverlands",
+        src: "assets/music/15_the_riverlands.flac"
+    },
+    {
+        title: "The Cliftlands",
+        src: "assets/music/16_the_cliftlands.flac"
+    },
+    {
+        title: "The Woodlands",
+        src: "assets/music/17_the_woodlands.flac"
+    }
+]
