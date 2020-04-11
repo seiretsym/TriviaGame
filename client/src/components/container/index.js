33077021f6ebbe7caede3e04b6099654d0722657
Kerwin Hy <kerwin.hy@gmail.com>
2020-04-11 00:11:05 -0700

    converted components to classes. added header and grid (row, col) components

--- client/src/components/container/index.js
+++ client/src/components/container/index.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Container = ({ children }) => {
-  return (
-    <div className="container rounded mt-5 border border-dark">
-      {children}
-    </div>
-  )
+class Container extends Component {
+  render() {
+    return (
+      <div className="container rounded mt-5 border border-dark">
+        {this.props.children}
+      </div>
+    )
+  };
 }
 
 export default Container;
--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import Container from "../components/container";
+import Header from "../components/header";
+import { Row, Col } from "../components/grid";
 
-const Main = () => {
-  return (
-    <Container>
-      <div className="row">
-        <div className="col-11 p-5 mx-auto">
-          <div className="row ">
-            <div className="col-12 bg p-3 text-center rounded border border-dark">
-              <h1>Octopath Traveler: Music Trivia</h1>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12 bg p-3 text-center rounded border border-dark mt-3">
-            </div>
-          </div>
-        </div>
-      </div>
-    </Container>
-  );
+class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col classes="-11 p-5 mx-auto">
+            <Row>
+              <Header />
+            </Row>
+            <Row classes="mt-3">
+              <Col classes="-12 bg p-3 text-center rounded border border-dark mt-3">
+                test
+            </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    )
+  };
 }
 
 export default Main;
