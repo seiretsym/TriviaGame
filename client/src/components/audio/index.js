6d003e4439536297ef4d5a4060a5d444e066b5ec
Kerwin Hy <kerwin.hy@gmail.com>
2020-04-12 11:40:15 -0700

    add more specifics to audio component

--- client/src/components/audio/index.js
+++ client/src/components/audio/index.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Audio extends Component {
   render() {
     return (
-      <audio src={this.props.src} {...this.props} />
+      <audio {...this.props}>
+        <source src={this.props.src} type="audio/wav" />
+      </audio>
     );
   }
 }
