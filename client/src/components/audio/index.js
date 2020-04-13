9d91be39e9c04e2953fe208626bcb025a4db5684
Kerwin Hy <kerwin.hy@gmail.com>
2020-04-13 12:30:41 -0700

    1. re-encode music files to mp3 from wav 2. updated audio component to load src as audio/mp3

--- client/src/components/audio/index.js
+++ client/src/components/audio/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Audio extends Component {
   render() {
     return (
-      <audio src={this.props.src} controls controlsList="nodownload" type="audio/wav" {...this.props} />
+      <audio src={this.props.src} controls controlsList="nodownload" type="audio/mp3" {...this.props} />
     );
   }
 }
